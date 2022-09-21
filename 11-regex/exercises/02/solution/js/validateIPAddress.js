896df045b032f7d47b1bdf0775208f56550598c8
GergelyPorvai <porvai.gergely@gmail.com>
2022-09-21 02:38:28 +0200

    v1

--- 11-regex/exercises/02/solution/js/validateIPAddress.js
+++ 11-regex/exercises/02/solution/js/validateIPAddress.js
@@ -1,1 +1,11 @@
+/* 2. Írj egy függvényt `validateIPAddress`,
+ami paraméterként egy IPv4 címet vár stringként!
+A függvény egy `Boolean` értékben visszaadja,
+hogy a paraméterként megadott IP cím megfelelő formátumú e, vagy sem. */
+
+const validateIPAddress = (IPv4 = "string") => {
+  const pattern = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+  return !!IPv4.match(pattern);
+};
+
 export default validateIPAddress;
